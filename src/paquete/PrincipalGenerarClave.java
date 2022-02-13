d2f131c94920ac561f1e017a10274bdc797850e8
Tadeo <Tadeo@DESKTOP-SP1GOU8>
2022-02-13 10:42:01 -0600

    Comentario Exitoso de git

--- src/paquete/PrincipalGenerarClave.java
+++ src/paquete/PrincipalGenerarClave.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package paquete;
-
+//Prueba de que subio git
 public class PrincipalGenerarClave {
     public static void main(String[] args) {
         GenerarClave clave = new GenerarClave();
