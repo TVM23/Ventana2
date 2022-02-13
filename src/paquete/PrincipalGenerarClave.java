2f5d5c712ceac17c805619ba1f4f3b1529456aa0
Tadeo <Tadeo@DESKTOP-SP1GOU8>
2022-02-13 10:11:11 -0600

    Subida de proyecto a git

---
+++ src/paquete/PrincipalGenerarClave.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete;
+
+public class PrincipalGenerarClave {
+    public static void main(String[] args) {
+        GenerarClave clave = new GenerarClave();
+        clave.Mostrar();
+    }
+}
